1cd134eddddfac5cf46607e1a6673114c2849bde
golb4212-design <golb4212@gmail.com>
2026-07-15 21:03:19 +0330

    Add files via upload

--- processor/worker.js
+++ processor/worker.js
@@ -1,11 +1,11 @@
 /* BLUEPANEL_PROCESSOR_WORKER
  * Fully split BluePanel runtime.
- * Version: 3.3.2
+ * Version: 3.3.4
  * Generated from the last stable 2.9.0 codebase.
  * Extracted application declarations: 88954 bytes.
  */
 
-const APP_VERSION = "3.3.2";
+const APP_VERSION = "3.3.4";
 
 const RESELLER_BACKUP_FIELDS = Object.freeze([
   "brand_name","welcome_text","support_username","card_holder","card_number","bank_name","iban",
@@ -1972,7 +1972,7 @@ async function ensureDb(env) {
   return true;
 }
 
-const BLUEPANEL_PROCESSOR_VERSION='3.3.2';
+const BLUEPANEL_PROCESSOR_VERSION='3.3.4';
 let processorSchemaPromise=null;
 function processorJson(data,status=200,headers={}){return new Response(JSON.stringify(data),{status,headers:{'content-type':'application/json; charset=utf-8','cache-control':'no-store',...headers}})}
 
